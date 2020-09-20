6144a6cc21eb25cfab29a6781f697b79249a2eba
Miko Kari <miko.kari@outlook.com>
2020-09-20 11:41:38 +0300

    typescripting

--- client/src/input/TextInput.tsx
+++ client/src/input/TextInput.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 
-class TextInput extends React.Component {
-  onChange = (event) => {
+type TextInputProps = {
+  label?: string;
+  value?: string;
+  error?: string;
+  onChange: (value: string) => void;
+};
+class TextInput extends React.Component<TextInputProps> {
+  onChange = (event: any) => {
     const { onChange } = this.props;
     if (onChange) {
       onChange(event.target.value);
